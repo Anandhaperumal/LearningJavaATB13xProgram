260b68d57f368bb6eb80faadc38f267b36269b7d
Anandha Perumal <anandha.anandhaa@gmail.com>
2025-06-30 14:17:44 +0530

    New adn Lab001 added

---
+++ src/ex_01_Java_Basic/Lab_002_Shortcut.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab_002_Shortcut {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("i");
+        System.out.println("Hi");
+        System.out.println("i");
+    }
+}
